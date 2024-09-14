6bef4566ecb8bfe6c165423605e7418ee2ed0e0e
Radcabna <radcabna@gmail.com>
2024-09-14 12:39:37 +0300

    Initial Commit

---
+++ DevRush 1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DevRush 1
+//
+//  Created by Алкександр Степанов on 14.09.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DevRush 1/DevRush_1App.swift
@@ -0,0 +1,17 @@
+//
+//  DevRush_1App.swift
+//  DevRush 1
+//
+//  Created by Алкександр Степанов on 14.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct DevRush_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
